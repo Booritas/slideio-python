5c300dbd9a5a2ee78b9538270eed907740567f77
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2025-01-19 15:45:49 +0100

    Windows ci

--- lib.ps1
+++ lib.ps1
@@ -123,45 +123,36 @@ function Build-Wheels {
         Create-CondaEnv -version $version
         Activate-CondaEnv -version $version
         python -m pip install numpy
-        try {
 
-            $pythonPath = (Get-Command python).Source
-            $pythonDir = Split-Path -Parent $pythonPath
-            $pythonLibDir = Join-Path $pythonDir "libs"
-    
-            $env:PYTHON_EXECUTABLE = $pythonPath
-            $env:Python3_EXECUTABLE = $pythonPath
-            $pythonVersion = (python --version).Split()[1]
-            $env:Python3_VERSION = $pythonVersion
-            $env:Python3_INCLUDE_DIRS = Join-Path $pythonDir "include"
-            $env:Python3_LIBRARY_DIRS = $pythonLibDir
-            $env:Python3_NumPy_INCLUDE_DIRS = python -c "import numpy; print(numpy.get_include())"
-            $env:Python3_LIBRARIES = Join-Path $pythonLibDir "python$($version.Replace('.', '')).lib"
-            $env:PYTHON_LOCATION_IS_SET = 1
-    
-            Write-Host "Python path: $pythonPath"
-            Write-Host "Python directory: $pythonDir"        
-            
-            # Build distributable
-            Remove-Item -Path .\build -Recurse -Force -ErrorAction SilentlyContinue
-            Write-Host "Executing command in conda environment for Python $version"
-            python --version
-            Write-Host "Installing wheel in conda environment for Python $version"
-            python -m pip install wheel
-            python -m pip install conan
+        $pythonPath = (Get-Command python).Source
+        $pythonDir = Split-Path -Parent $pythonPath
+        $pythonLibDir = Join-Path $pythonDir "libs"
+
+        $env:PYTHON_EXECUTABLE = $pythonPath
+        $env:Python3_EXECUTABLE = $pythonPath
+        $pythonVersion = (python --version).Split()[1]
+        $env:Python3_VERSION = $pythonVersion
+        $env:Python3_INCLUDE_DIRS = Join-Path $pythonDir "include"
+        $env:Python3_LIBRARY_DIRS = $pythonLibDir
+        $env:Python3_NumPy_INCLUDE_DIRS = python -c "import numpy; print(numpy.get_include())"
+        $env:Python3_LIBRARIES = Join-Path $pythonLibDir "python$($version.Replace('.', '')).lib"
+        $env:PYTHON_LOCATION_IS_SET = 1
+
+        Write-Host "Python path: $pythonPath"
+        Write-Host "Python directory: $pythonDir"        
         
-            python setup.py sdist bdist_wheel
-            Get-ChildItem -Path .\dist
-        }
-        catch {
-            Write-Host "An error occurred: $_"
-            return
-        }
-        finally {
-            Deactivate-CondaEnv
-            Remove-CondaEnv -version $version
-        }
-
+        # Build distributable
+        Remove-Item -Path .\build -Recurse -Force -ErrorAction SilentlyContinue
+        Write-Host "Executing command in conda environment for Python $version"
+        python --version
+        Write-Host "Installing wheel in conda environment for Python $version"
+        python -m pip install wheel
+        python -m pip install conan
+    
+        python setup.py sdist bdist_wheel
+        Get-ChildItem -Path .\dist
+        Deactivate-CondaEnv
+        Remove-CondaEnv -version $version
     }
         
 }
