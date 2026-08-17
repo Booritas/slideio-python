e53ff7a630f8877ea4c43e4ad1982946a82921b4
Booritas <stanislav.melnikov@gmail.com>
2026-08-17 15:04:21 +0200

    increase version of the python package to 2.9.0

--- build-dependencies.ps1
+++ build-dependencies.ps1
@@ -59,7 +59,7 @@ function Invoke-ConanCreateSlideio {
 }
 
 try {
-    Invoke-ConanCreateSlideio -FolderPath "recipes\slideio\all" -Version "2.8.1"
+    Invoke-ConanCreateSlideio -FolderPath "recipes\slideio\all" -Version "2.9.0"
 }
 catch {
     Write-Error "An error occurred: $_"
--- build-dependencies.sh
+++ build-dependencies.sh
@@ -115,7 +115,7 @@ invoke_conan_create_slideio() {
 original_dir=$(pwd)
 
 create_conan_recipes() {
-    invoke_conan_create_slideio "recipes/slideio/all" "2.8.0"
+    invoke_conan_create_slideio "recipes/slideio/all" "2.9.0"
 }
 
 # Call the function with the appropriate profile
